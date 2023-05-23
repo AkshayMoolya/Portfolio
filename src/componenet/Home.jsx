ca64fcb13f738535e4d2c05089cecd8b2bf5e95c
Akshay Moolya <akshaymoolya88@gmail.com>
2023-05-23 13:59:14 +0530

    made some changes

--- src/componenet/Home.jsx
+++ src/componenet/Home.jsx
@@ -12,9 +12,9 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Full Stack Developer.
           </h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="text-gray-500 py-4 max-w-lg font-bold">
             I'm a full-stack web developer from India named Akshay Moolya . My
             interests are coding, open source, and the online platform. I've
             worked with a variety of technologies, including MongoDB, React, and
@@ -34,13 +34,6 @@ const Home = () => {
               </span>
             </Link>
           </div>
-        </div>
-        <div>
-          <img
-            src={hero}
-            alt=""
-            className="rounded-2xl mx-auto w-2/3 md:h-full "
-          />
         </div>
       </div>
     </div>
--- src/componenet/Portfolio.jsx
+++ src/componenet/Portfolio.jsx
@@ -1,54 +1,50 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import youtube from "../assets/portfolio/youtube.png";
+import weatherapp from "../assets/portfolio/Weather app.png";
+import crypto from "../assets/portfolio/CryptoTracker.png";
+import Moviehub from "../assets/portfolio/MovieHub.png";
 
 const Portfolio = () => {
   const portfolio = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: youtube,
+      demo: "https://youtube-clone-five-omega.vercel.app/",
+      code: "https://github.com/AkshayMoolya/youtube-clone",
     },
     {
       id: 2,
-      src: installNode,
+      src: weatherapp,
+      demo: "https://weather-app-theta-amber.vercel.app/",
+      code: "https://github.com/AkshayMoolya/Weather-app",
     },
     {
       id: 3,
-      src: navbar,
+      src: Moviehub,
+      demo: "https://movie-hub-tau.vercel.app/",
+      code: "https://github.com/AkshayMoolya/MovieHub",
     },
     {
       id: 4,
-      src: reactParallax,
-    },
-    {
-      id: 5,
-      src: reactSmooth,
-    },
-    {
-      id: 6,
-      src: reactWeather,
+      src: crypto,
+      demo: "https://crypto-tracker-ten-iota.vercel.app/",
+      code: "https://github.com/AkshayMoolya/Crypto-tracker",
     },
   ];
   return (
-    <div  
+    <div
       name="portfolio"
       className="bg-gradient-to-b  from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="py-6">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
+            Projects
           </p>
-          <p className="py-6 ">
-          Check out some of my work right her
-          </p>
+          <p className="py-6 ">Check out some of my work right her</p>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3  gap-8 px-12 sm:px-0 justify-center ">
-          {portfolio.map(({ id, src }) => (
+          {portfolio.map(({ id, src, demo, code }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -57,10 +53,16 @@ const Portfolio = () => {
               />
 
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => window.open(`${demo}`, "_blank")}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   demo
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  onClick={() => window.open(`${code}`, "_blank")}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
                   code
                 </button>
               </div>
