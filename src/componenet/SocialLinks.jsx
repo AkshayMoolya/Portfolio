e8d24e97a1c628843d3e4a835e66c38a714d86dc
Akshay Moolya <akshaymoolya88@gmail.com>
2023-06-10 11:24:41 +0530

    made some changes

--- src/componenet/SocialLinks.jsx
+++ src/componenet/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "../assets/portfolio/Akshay.pdf ",
+      href: "/Akshay.pdf",
       style: "rounded-br-md",
       download: true,
     },
